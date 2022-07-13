6a0f7933f9a582ee43619a7921fc3aa6c2c75daf
Alan Halatian <alanhalatian@gmail.com>
2022-07-13 00:41:04 -0300

    Start assembler

---
+++ 06/assembler/lib/assembler.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "assembler/version"
+require_relative "assembler/lexer"
+require_relative "assembler/parser"
+
+module Assembler
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ 06/assembler/spec/assembler_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Assembler do
+  it "has a version number" do
+    expect(Assembler::VERSION).not_to be nil
+  end
+end
